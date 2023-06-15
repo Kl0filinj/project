a430b77a57dfdafb65b629b81a33dffe7c2a2025
Daniil Velychko <vladdanya08@gmail.com>
2023-06-16 00:51:13 +0200

    config users module, dockerize auth app, add a jwt config into auth.module

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -1,4 +1,29 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { DatabaseModule } from '@app/common/database';
+import { UserDocument, UserSchema } from './models/users.schema';
+import { LoggerModule } from 'nestjs-pino';
+import { UsersRepository } from './users.repository';
 
-@Module({})
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      { name: UserDocument.name, schema: UserSchema },
+    ]),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService, UsersRepository],
+})
 export class UsersModule {}
