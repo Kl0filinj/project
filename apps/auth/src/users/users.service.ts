75f4b4936acdc828dc3811ee4c7e4a0064ce8b5b
Daniil Velychko <vladdanya08@gmail.com>
2023-06-16 15:24:18 +0200

    setting up passport guard

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcrypt from 'bcrypt';
@@ -14,5 +14,13 @@ export class UsersService {
     });
   }
 
-  async validateUser(email: string, password: string) {}
+  async verifyUser(email: string, password: string) {
+    const user = await this.usersRepository.findOne({ email });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('credentials are wrong');
+    }
+    return user;
+  }
 }
