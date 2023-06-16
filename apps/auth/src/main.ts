5913e0c9543e8acf2346cd450f05b040a0a0ee64
Daniil Velychko <vladdanya08@gmail.com>
2023-06-16 12:02:20 +0200

    users & passport config

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(3001);
+  const confirService = app.get(ConfigService);
+  await app.listen(confirService.get('PORT'));
 }
 bootstrap();
